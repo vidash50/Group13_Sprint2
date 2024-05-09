7191b0071461e143f36965559bd1176dc6500abe
vidaa <vidash50@gmail.com>
2024-05-09 12:16:08 -0500

    Unversioned files added...

--- src/test/java/com/finasystems/runners/CukesRunner.java
+++ src/test/java/com/finasystems/runners/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/finasystems/step_definitions",
         dryRun = false,
-        tags = "@wip",
+        tags = "@dekked",
         publish = true
 )
 

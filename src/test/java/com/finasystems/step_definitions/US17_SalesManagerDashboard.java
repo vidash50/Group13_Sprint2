2f24252f91dd8a95d3b3bff857acab99c4008b0c
Lilluchee <lilluchee@gmail.com>
2024-05-19 00:47:24 -0500

    US 17 worked and almost at the finish line

--- src/test/java/com/finasystems/step_definitions/US17_SalesManagerDashboard.java
+++ src/test/java/com/finasystems/step_definitions/US17_SalesManagerDashboard.java
@@ -26,23 +26,52 @@ public class US17_SalesManagerDashboard {
     }
 
 
-    @Then("user sees all modules on the dashboard")
+    @When("user sees all modules on the dashboard")
+    public void userSeesAllModulesOnTheDashboard() {
+       //salesManagerDashboardPage.dashboardModules.size();
+    }
 
-    public void userSeesAllModulesOnTheDashboard(List<String> expectedModules) {
-List<String> actualModuleNames =new ArrayList<String>();
+    @Then("user clicks on each module on the dashboard")
+    public void userClicksOnEachModuleOnTheDashboard() {
 
-        for (WebElement each : salesManagerDashboardPage.dashboardModule) {
-            actualModuleNames.add(each.getText());
-        }
-        System.out.println("actualModuleNames = " + actualModuleNames);
+        salesManagerDashboardPage.discussBtn.click();
+        salesManagerDashboardPage.calendarBtn.click();
+        salesManagerDashboardPage.notesBtn.click();
+        salesManagerDashboardPage.contactBtn.click();
+        salesManagerDashboardPage.crmBtn.click();
+        salesManagerDashboardPage.salesBtn.click();
+        salesManagerDashboardPage.websiteBtn.click();
+        salesManagerDashboardPage.pointOfSaleBtn.click();
+        salesManagerDashboardPage.purchaseBtn.click();
+        salesManagerDashboardPage.inventoryBtn.click();
+        salesManagerDashboardPage.repairsBtn.click();
+        salesManagerDashboardPage.invoicingBtn.click();
+        salesManagerDashboardPage.emailMarketingBtn.click();
+        salesManagerDashboardPage.eventsBtn.click();
+        salesManagerDashboardPage.employeesBtn.click();
+        salesManagerDashboardPage.leavesBtn.click();
+        salesManagerDashboardPage.moreBtn.click();
+        BrowserUtils.waitFor(2);
+        salesManagerDashboardPage.expensesBtn.click();
+        salesManagerDashboardPage.moreBtn.click();
+        salesManagerDashboardPage.maintenanceBtn.click();
+        salesManagerDashboardPage.moreBtn.click();
+        salesManagerDashboardPage.dashboardsBtn.click();
+    }
+}
+
+//    public void userSeesAllModulesOnTheDashboard(List<String> expectedModules) {
+//List<String> actualModuleNames =new ArrayList<String>();
+//
+//        for (WebElement each : salesManagerDashboardPage.dashboardModule) {
+//            actualModuleNames.add(each.getText());
+//        }
+//        System.out.println("actualModuleNames = " + actualModuleNames);
 
      //   Assert.assertTrue(actualModuleNames.equals(expectedModules));
 
-
-
-    }
 //    @Then("user sees all {int} modules on the dashboard")
 //    public void user_sees_all_modules_on_the_dashboard(Integer int1) {
 
 
-}
+
--- src/test/resources/features/US17_SalesManagerDashboardModules.feature
+++ src/test/resources/features/US17_SalesManagerDashboardModules.feature
@@ -9,25 +9,7 @@ Feature: Default
 	@B28-220
 	Scenario: As a sales manager, I should be able to access the different modules.
 		Given user is on sales manager profile
-		#When user is on sales manager dashboard
-		Then user sees all modules on the dashboard
+		#When user sees all modules on the dashboard
+		Then user clicks on each module on the dashboard
 
-		|Discuss|
-		|Calendar|
-		|Notes|
-		|Contacts|
-		|CRM|
-		|Sales|
-		|Website|
-		|Point of Sale|
-		|Purchases|
-		|Inventory|
-		|Repairs|
-		|Invoicing|
-		|Email Marketing|
-		|Events|
-		|Employees|
-		|Leaves|
-		|Expenses|
-		|Maintenance|
-		|Dashboards|
+
